7e79224b3f9022a5aa4ea41bf22e2b68efbf2db6
Satish Singh <86511164+satishSingh0710@users.noreply.github.com>
2023-10-16 14:21:27 +0530

    Update sievePrimeFactorization.cpp

--- sievePrimeFactorization.cpp
+++ sievePrimeFactorization.cpp
@@ -1,1 +1,17 @@
-
+int ar[1000001];
+void sieve(){
+    int maxN = 1000000; 
+    for (int i = 1;i<=maxN;i++){
+        ar[i] = -1; 
+    }
+    
+    for (int i = 2;i<=maxN;i++){
+        if(ar[i] == -1){
+            for (int j = i;j<=maxN;j+=i){
+                if(ar[j] == -1){
+                    ar[j] = i; 
+                }
+            }
+        }
+    }
+}
